287245a71635c41e158316a6ecbad37e3c8b1be8
고동현 <dhjk35@naver.com>
2023-03-21 02:28:15 +0900

    refactor(pxToRem): rem 변환을 위해 브라우저의 폰트 크기를 가져와서 변경하도록 수정

--- src/utils/pxToRem.ts
+++ src/utils/pxToRem.ts
@@ -1,5 +1,10 @@
-const pxToRem = (pixel: number, base = 16): string => {
-  return `${pixel / base}rem`;
+const pxToRem = (pixel: number): string => {
+  const rootFontSize = parseFloat(
+    getComputedStyle(document.documentElement).fontSize
+  );
+  const remValue = pixel / (rootFontSize || 16);
+
+  return `${remValue}rem`;
 };
 
 export default pxToRem;
