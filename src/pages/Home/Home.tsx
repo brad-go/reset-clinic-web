2427896d7b82331d76e5ab63f62e4e5e5469cb4a
고동현 <dhjk35@naver.com>
2023-03-24 13:00:27 +0900

    feat(Home): 버튼 클릭시 데이터를 보내고 video페이지로 이동하는 기능 구현

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,13 +1,26 @@
-import { useAppSelector } from '@/hooks';
+import { NavLink } from 'react-router-dom';
+
 import { Trigger } from '@/components';
+import { useAppSelector } from '@/hooks';
+import { requestUserData } from '@/services';
+import { getLocalDateString } from '@/utils';
 
 import { RootState } from '@/store';
 import * as S from './Home.styles';
 
 const Home = () => {
-  const { nickname } = useAppSelector((state: RootState) => state.user);
+  const { visitorId, nickname } = useAppSelector(
+    (state: RootState) => state.user
+  );
 
-  const handleClick = () => {};
+  const handleClick = () => {
+    const requestData = {
+      visitorId,
+      at: getLocalDateString(),
+    };
+
+    requestUserData('/button-click', requestData);
+  };
 
   return (
     <S.Container>
@@ -16,7 +29,9 @@ const Home = () => {
           {nickname}님 잘 오셨어요! 오늘 하루도 금연 화이팅 합시다!
         </S.Header>
       )}
-      <Trigger onClick={handleClick}>Press</Trigger>
+      <NavLink to="/video">
+        <Trigger onClick={handleClick}>Press</Trigger>
+      </NavLink>
       <S.Footer>담배가 땡긴다면 눌러보세요!</S.Footer>
     </S.Container>
   );
