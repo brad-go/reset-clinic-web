07e0402c75349c7688b6990fc008bc1dcc85cf29
고동현 <dhjk35@naver.com>
2023-03-24 12:59:00 +0900

    feat: 유저에 맞는 영상을 가져오는 기능 구현

--- src/services/user.ts
+++ src/services/user.ts
@@ -1,6 +1,10 @@
 import axios, { AxiosRequestConfig } from 'axios';
 
-import type { RequestData } from '@/types';
+import { VIDEOS } from '@/constants';
+import { getRandomNumberWithRange } from '@/utils';
+
+import type { User, RequestData } from '@/types';
+import type { Video, VideoType } from '@/constants/video';
 
 const axiosConfig: AxiosRequestConfig = {
   baseURL: import.meta.env.VITE_API_END_POINT,
@@ -8,7 +12,7 @@ const axiosConfig: AxiosRequestConfig = {
 
 const instance = axios.create(axiosConfig);
 
-const requestUserData = async (url: string, data: RequestData) => {
+export const requestUserData = async (url: string, data: RequestData) => {
   try {
     const response = await instance.post(url, data);
 
@@ -18,4 +22,17 @@ const requestUserData = async (url: string, data: RequestData) => {
   }
 };
 
-export default requestUserData;
+const getRandomVideo = (videos: Video[], videoType: VideoType) => {
+  const preferedVideos = videos.filter(({ type }) => type === videoType);
+  const index = getRandomNumberWithRange(0, preferedVideos.length - 1);
+
+  return preferedVideos[index];
+};
+
+export const findUserPreferedVideo = (user: User) => {
+  if (!user.preference) {
+    return getRandomVideo(VIDEOS, 'NEW');
+  }
+
+  return getRandomVideo(VIDEOS, user.preference);
+};
